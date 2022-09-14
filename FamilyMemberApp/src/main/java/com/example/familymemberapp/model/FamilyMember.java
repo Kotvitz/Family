39831d7318975ebbeb582807b0c074bc79e67056
Michał Kotfica <kotvitz17@gmail.com>
2022-09-14 10:48:44 +0200

    create family member app module and model

---
+++ FamilyMemberApp/src/main/java/com/example/familymemberapp/model/FamilyMember.java
@@ -0,0 +1,8 @@
+package com.example.familymemberapp.model;
+
+public class FamilyMember {
+    private Long familyId;
+    private String familyName;
+    private String givenName;
+    private Integer age;
+}
