fa07b6ab993d64098b81c693888e88cd9c690373
Michał Kotfica <kotvitz17@gmail.com>
2022-09-14 10:37:10 +0200

    rename some family app classes

--- FamilyApp/src/main/java/com/example/familyapp/FamilyApp.java
+++ FamilyApp/src/main/java/com/example/familyapp/FamilyApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreateFamily {
+public class FamilyApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreateFamily.class, args);
+		SpringApplication.run(FamilyApp.class, args);
 	}
 
 }
