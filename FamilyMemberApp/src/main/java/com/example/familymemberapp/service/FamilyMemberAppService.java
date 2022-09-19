ec5b98a00c6151fe733bd28a27a42fd5afbb31fe
Michał Kotfica <kotvitz17@gmail.com>
2022-09-19 23:33:47 +0200

    improve class file structure

--- FamilyMemberApp/src/main/java/com/example/familymemberapp/service/FamilyMemberAppService.java
+++ FamilyMemberApp/src/main/java/com/example/familymemberapp/service/FamilyMemberAppService.java
@@ -13,37 +13,37 @@ import com.example.familymemberapp.model.FamilyMember;
 
 @Service
 public class FamilyMemberAppService {
-	
-	private static Logger logger = LogManager.getLogger(FamilyMemberAppService.class);
-	
-	@Value("${familyMemberAppService.createMember}")
-    private String uriCreateMember;
 
-    @Value("${familyMemberAppService.searchMember}")
-    private String uriSearchMember;
-    
-    private final RestTemplate restTemplate;
-    
-    @Autowired
-    public FamilyMemberAppService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-    
-    public Boolean createFamilyMember(FamilyMember familyMember) {
+	private static Logger logger = LogManager.getLogger(FamilyMemberAppService.class);
+
+	@Value("${familyMemberAppService.createMember}")
+	private String uriCreateMember;
+
+	@Value("${familyMemberAppService.searchMember}")
+	private String uriSearchMember;
+
+	private final RestTemplate restTemplate;
+
+	@Autowired
+	public FamilyMemberAppService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public Boolean createFamilyMember(FamilyMember familyMember) {
 		try {
 			restTemplate.postForObject(uriCreateMember, familyMember, FamilyMember.class);
 			logger.info("Request was sent to service: " + uriCreateMember);
 			return true;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			logger.error("Call of service " + uriCreateMember + " has failed.");
 			logger.error(e.getMessage());
 			return false;
 		}
-    }
-    
-    public List<FamilyMember> searchFamilyMember(Long id) {
-    	List<FamilyMember> memberList = restTemplate.getForObject(uriSearchMember + id.toString(), List.class);
-    	logger.info("Data of family with ID " + id.toString() + " was received.");
+	}
+
+	public List<FamilyMember> searchFamilyMember(Long id) {
+		List<FamilyMember> memberList = restTemplate.getForObject(uriSearchMember + id.toString(), List.class);
+		logger.info("Data of family with ID " + id.toString() + " was received.");
 		return memberList;
-    }
+	}
 }
