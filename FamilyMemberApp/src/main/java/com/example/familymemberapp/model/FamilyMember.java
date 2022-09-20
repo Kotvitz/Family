350f97c2b1aab6023e807962c5cc1b0727026446
Michał Kotfica <kotvitz17@gmail.com>
2022-09-20 23:53:42 +0200

    change of variable type in family member class

--- FamilyMemberApp/src/main/java/com/example/familymemberapp/model/FamilyMember.java
+++ FamilyMemberApp/src/main/java/com/example/familymemberapp/model/FamilyMember.java
@@ -1,8 +1,51 @@
 package com.example.familymemberapp.model;
 
 public class FamilyMember {
-    private Long familyId;
-    private String familyName;
-    private String givenName;
-    private Integer age;
+	private Long familyId;
+	private String familyName;
+	private String givenName;
+	private int age;
+
+	public FamilyMember() {
+
+	}
+
+	public FamilyMember(Long familyId, String familyName, String givenName, int age) {
+		this.setFamilyId(familyId);
+		this.setFamilyName(familyName);
+		this.setGivenName(givenName);
+		this.setAge(age);
+	}
+
+	public Long getFamilyId() {
+		return familyId;
+	}
+
+	public void setFamilyId(Long familyId) {
+		this.familyId = familyId;
+	}
+
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+
+	public String getGivenName() {
+		return givenName;
+	}
+
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
