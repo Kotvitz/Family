e09964c3be15785e762604076c20db6c74640f1b
Michał Kotfica <kotvitz17@gmail.com>
2022-09-24 21:15:52 +0200

    add rest template bean to spring boot applications

--- FamilyMemberApp/src/main/java/com/example/familymemberapp/FamilyMemberApp.java
+++ FamilyMemberApp/src/main/java/com/example/familymemberapp/FamilyMemberApp.java
@@ -2,6 +2,8 @@ package com.example.familymemberapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FamilyMemberApp {
@@ -10,4 +12,8 @@ public class FamilyMemberApp {
 		SpringApplication.run(FamilyMemberApp.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
